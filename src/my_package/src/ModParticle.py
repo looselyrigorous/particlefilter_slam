Fixed odometry huge error in clock-wise rotation :)

src/my_package/src/ModParticle.py
@@ -8,20 +8,20 @@ from math import atan2
 #import Preprocess as pp
 
 class Particle:
-    a1 = 0.001
-    a2 = 0.001
-    a3 = 0.001
-    a4 = 0.001
+    a1 = 0.0000000000001
+    a2 = 0.0000000000001
+    a3 = 0.0000000001
+    a4 = 0.0000000001
     count = 0
 
-    fidelity = 0.1  # this is the map fidelity(e.g 0.1 means every pixel is 10cm * 10cm)
+    fidelity = 0.2  # this is the map fidelity(e.g 0.1 means every pixel is 10cm * 10cm)
     genX = 0.0  # this is the ros prediction for its pos
     genY = 0.0
     genTH = 0.0
     NoP = 1  # Number of Particles
     hitmap = None
-    sizeX = 301
-    sizeY = 501
+    sizeX = 201
+    sizeY = 201
 
     def __init__(self, x, y, th, grid, prop, prop_map, tick_map):
         self.x = x
@@ -40,32 +40,41 @@ class Particle:
         a3 = Particle.a3
         a4 = Particle.a4
 
+
         sgm1 = abs(a1 * drot1 + a2 * dtrans)
         sgm2 = abs(a3 * dtrans + a4 * (drot2 + drot1))
         sgm3 = abs(a1 * drot2 + a2 * dtrans)
 
         dev1 = normal(0, sgm1)
+        #print(dev1)
         dev2 = normal(0, sgm2)
+
         dev3 = normal(0, sgm3)
+        #print(dev3)
 
         drote1 = drot1 + dev1
         dtranse = dtrans + dev2
         drote2 = drot2 + dev3
 
+
         self.x += dtranse * cos(self.th + drote1)
         self.y += dtranse * sin(self.th + drote1)
-        self.th += drote1 + drote2
-        self.th = self.th % (2*np.pi)
-        print (norm(0, sgm1).pdf(dev1) * norm(0, sgm2).pdf(dev2) * norm(0, sgm3).pdf(dev3))
-        self.prop *= ((norm(0, sgm1).pdf(dev1)*a1) * (norm(0, sgm2).pdf(dev2)*a1) * (norm(0, sgm3).pdf(dev3)*a1))
+        self.th += (drote1 + drote2)
+        self.th = self.th
 
+        #print(drote1 + drote2)
+
+        normalizing_factor = (norm(0, sgm1).pdf(0)) * (norm(0, sgm2).pdf(0)) * (norm(0, sgm3).pdf(0))
+        self.prop *= (norm(0, sgm1).pdf(dev1)) * (norm(0, sgm2).pdf(dev2)) * (norm(0, sgm3).pdf(dev3))/normalizing_factor
+
+        print(self.th)
     # The function must have in
 
     def print_map(self):
         if Particle.count == 0:
             mb.print_map(self.grid)
         Particle.count += 1
-        if Particle.count == 21:
+        if Particle.count == 101:
             Particle.count = 0
 
     def merge_map_maker(self, func, start_angle, end_angle, angle_incr, ranges, max_depth):
@@ -75,18 +84,13 @@ class Particle:
         return merger_map
 
     def prop_map_maker(self, func, merger_map):
-        #print(type(self.prop_map))
         self.prop_map = func(self.prop_map, self.tick_map, merger_map)
-        #print(type(self.prop_map))
-
+        
     def grid_maker(self, func):
         self.grid = func(self.prop_map)
-        #self.print_map()
 
     def map_error(self,func,merger_map):
-        print(self.prop)
         self.prop *= func(merger_map,self.grid,self.prop_map,self.tick_map)
-        print(self.prop)
 
     def normalize(self, ammount):
         self.prop /= ammount
@@ -107,11 +111,9 @@ class Particle:
 
 
 
-def init_particles():
+def init_particles(x,y,th):
     Particles = list()
-    x = 0
-    y = 0
-    th = 0
+    replace_gen_pos(x,y,th)
     grid = np.ndarray(shape=(Particle.sizeX, Particle.sizeY), dtype=np.int)
     grid.fill(-1)
     prop_map = np.ndarray(shape=(Particle.sizeX, Particle.sizeY), dtype=np.float)
@@ -119,7 +121,7 @@ def init_particles():
     tick_map = np.ndarray(shape=(Particle.sizeX, Particle.sizeY), dtype=np.int)
     tick_map.fill(0)
     for i in range(0, Particle.NoP):
-        Particles.append(Particle(x, y, th, grid, 1, prop_map, tick_map))
+        Particles.append(Particle(0, 0, 0, grid, 1, prop_map, tick_map))
     return Particles
 
 
@@ -132,11 +134,14 @@ def map_update(merge_map_func, prop_map_func, grid_make_func, map_error_func,
         p.map_error(map_error_func,merger_map)
         p.prop_map_maker(prop_map_func, merger_map)
         p.grid_maker(grid_make_func)
+    selectSurvivors(Particles)
     return merger_map
+
 
 
 def odom_update(Particles, X, Y, TH):
     drot1, dtrans, drot2 = calculate_diff(X, Y, TH)
+    #print(drot1 + drot2,Particle.genTH - TH)
 
     for i in Particles:
         i.move_particle(drot1, dtrans, drot2)
@@ -158,7 +163,6 @@ def normalizeAndLineUp(Particles):
     nextStartPoint = 0
     for p in Particles:
         nextStartPoint = p.line_up(nextStartPoint)
-    #print(sum)
 
 def selectSurvivors(Particles):
     normalizeAndLineUp(Particles)
@@ -184,14 +188,15 @@ def selectSurvivors(Particles):
     for s in survivors:
         children.append(s.procreate())
 
+    #print_best_particle(children)
+
     return children
 
 
 def calculate_diff(X, Y, TH):
     drot1 = atan2(Y - Particle.genY, X - Particle.genX) - Particle.genTH
     dtrans = math.sqrt((Particle.genX - X) ** 2 + (Particle.genY - Y) ** 2)
-    drot2 = TH - Particle.genTH - drot1
-
+    drot2 = (TH - Particle.genTH - drot1)
     return drot1, dtrans, drot2
 
 
@@ -221,4 +226,14 @@ def quaternion_to_radians(w, x, y, z):
     t4 = +1.0 - 2.0 * (ysqr + z * z)
     Z = math.degrees(math.atan2(t3, t4))
 
-    return np.deg2rad(X) % 2*np.pi, np.deg2rad(Y) % 2*np.pi, np.deg2rad(Z) % 2*np.pi
+    return np.deg2rad(X), np.deg2rad(Y), np.deg2rad(Z)
+
+
+def print_best_particle(Particles):
+    max = 0
+    best_particle = None
+    for i in Particles:
+        if max < i.prop:
+            max = i.prop
+            best_particle = i
+    best_particle.print_map()